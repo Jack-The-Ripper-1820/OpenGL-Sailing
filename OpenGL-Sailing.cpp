38ebc269555f434b517ddddeffc8c8cf30b7db6a
Mayur <mayurjreddy@gmail.com>
2023-08-30 17:19:58 -0400

    Initial code commit

---
+++ OpenGL-Sailing.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGL-Sailing.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGL-Sailing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
