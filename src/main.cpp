92d635f882915830cda0a3546b511a65b0f2be9a
Mayur <mayurjreddy@gmail.com>
2023-08-30 19:22:26 -0400

    added glew plus first sucessful hello world

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+﻿// OpenGL-Sailing.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <assimp/Importer.hpp>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
