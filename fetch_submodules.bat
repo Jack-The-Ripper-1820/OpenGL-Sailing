9dbf92be81798cc946991b7d23e60c072c769bcc
Mayur <mayurjreddy@gmail.com>
2023-10-10 00:04:09 -0400

    fetch_submodules.bat update

--- fetch_submodules.bat
+++ fetch_submodules.bat
@@ -1,13 +1,22 @@
 @echo off
 
+REM Initialize the repo as a Git repository
 git init
 
+REM Add the remote
 git remote add origin https://github.com/Jack-The-Ripper-1820/OpenGL-Sailing.git
 
-git pull origin main
+REM Fetch all the data from the repo (not changing any files in the working directory)
+git fetch origin
 
+REM Set the local branch to track the remote main branch without checking it out
+git branch --track main origin/main
+
+REM Update the submodules
 git submodule update --init --recursive
 
-git add . 
+REM Add all changes
+git add .
 
+REM Commit the changes
 git commit -m "local commit"
